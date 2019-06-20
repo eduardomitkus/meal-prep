25f3a3bf5553b4e9977a9ba1d10e3ac9123c67b4
Eduardo Mitkus <eduardo.silva.mitkus@gmail.com>
2019-06-20 17:44:12 -0500

    Adicionando estrutura para Planos de refeição

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,22 @@
 <template>
     <span>
-        <HomeHero />
-        <HomeDetails />
+        <homeHero />
+        <homeDetails />
+        <homePlans />
     </span>
 </template>
 
 <script>
 import HomeHero from '@/components/HomeHero'
 import HomeDetails from '@/components/HomeDetails'
+import HomePlans from '@/components/HomePlans'
 
 export default {
     name: 'home',
     components: {
         HomeHero,
-        HomeDetails
+        HomeDetails,
+        HomePlans
     }
 }
 </script>
